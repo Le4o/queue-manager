5081b93d1e37f300b353b0b0f965b8ac8909eda5
Le4o <devleonardoandrade@gmail.com>
2021-12-15 15:58:46 -0300

    chore: adding nestjs/bull

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: process.env.REDIS_HOST || 'localhost',
+        port: parseInt(process.env.REDIS_PORT, 10) || 6379,
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
